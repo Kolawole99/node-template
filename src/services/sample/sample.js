47dc1e179465330c53dfcb47ad5ad79f36039120
Kolawole Ojo <philliphopeojo@gmail.com>
2021-11-07 15:57:03 +0100

    Custom errors update

--- src/services/sample/sample.js
+++ src/services/sample/sample.js
@@ -1,14 +1,9 @@
-/**
- * @author Oguntuberu Nathan O. <nateoguns.work@gmail.com>
- * */
-const { NODE_ENV } = process.env;
 const RootService = require('../_root');
 const { buildQuery, buildWildcardOptions } = require('../../utilities/query');
 const { createSchema, updateSchema } = require('../../validators/sample');
 
 class SampleService extends RootService {
     constructor(sampleController) {
-        /** */
         super();
         this.sampleController = sampleController;
         this.serviceName = 'SampleService';
@@ -17,19 +12,15 @@ class SampleService extends RootService {
     async createRecord(request, next) {
         try {
             const { body } = request;
-            if (Object.keys(body).length === 0) throw new Error('Data is required to create.');
-
-            delete body.id;
-
             const { error } = createSchema.validate(body);
             if (error) throw new CustomValidationError(this.filterJOIValidation(error.message));
 
             const result = await this.sampleController.createRecord({ ...body });
-            if (result.failed) throw new Error(result.error);
+            if (result.failed) throw new CustomControllerError(result.error);
 
             return this.processSingleRead(result);
         } catch (e) {
-            const processedError = this.formatError(this.serviceName, e, 'createRecord');
+            let processedError = this.formatError(this.serviceName, e, 'createRecord');
             const err = this.processFailedResponse(
                 processedError.errorMessage,
                 processedError.statusCode
@@ -41,18 +32,18 @@ class SampleService extends RootService {
     async readRecordById(request, next) {
         try {
             const { id } = request.params;
-            if (!id) throw new Error('Invalid ID supplied.');
+            if (!id) throw new CustomValidationError('Invalid ID supplied.');
 
             const result = await this.sampleController.readRecords({ id, isActive: true });
-            if (result.failed) throw new Error(result.error);
+            if (result.failed) throw new CustomControllerError(result.error);
 
             return this.processSingleRead(result[0]);
         } catch (e) {
-            const errorMessage =
-                NODE_ENV === 'DEVELOPMENT'
-                    ? `[${this.serviceName}] readRecordById: ${e.message}`
-                    : e.message;
-            const err = this.processFailedResponse(errorMessage, 500);
+            let processedError = this.formatError(this.serviceName, e, 'readRecordById');
+            const err = this.processFailedResponse(
+                processedError.errorMessage,
+                processedError.statusCode
+            );
             return next(err);
         }
     }
@@ -60,18 +51,19 @@ class SampleService extends RootService {
     async readRecordsByFilter(request, next) {
         try {
             const { query } = request;
-            if (Object.keys(query).length === 0) throw new Error('Query is required to filter.');
+            if (Object.keys(query).length === 0)
+                throw new CustomValidationError('Query is required to filter.');
 
             const result = await this.handleDatabaseRead(this.sampleController, query);
-            if (result.failed) throw new Error(result.error);
+            if (result.failed) throw new CustomControllerError(result.error);
 
             return this.processMultipleReadResults(result);
         } catch (e) {
-            const errorMessage =
-                NODE_ENV === 'DEVELOPMENT'
-                    ? `[${this.serviceName}] readRecordsByFilter: ${e.message}`
-                    : e.message;
-            const err = this.processFailedResponse(errorMessage, 500);
+            let processedError = this.formatError(this.serviceName, e, 'readRecordsByFilter');
+            const err = this.processFailedResponse(
+                processedError.errorMessage,
+                processedError.statusCode
+            );
             return next(err);
         }
     }
@@ -79,12 +71,12 @@ class SampleService extends RootService {
     async readRecordsByWildcard(request, next) {
         try {
             const { params, query } = request;
-            if (!params || !query) throw new Error('Invalid key/keyword', 400);
+            if (!params || !query) throw new CustomValidationError('Invalid key/keyword');
             if (Object.keys(params).length === 0) {
-                throw new Error('Keys are required to read', 400);
+                throw new CustomValidationError('Keys are required to read');
             }
             if (Object.keys(query).length === 0) {
-                throw new Error('Keywords are required to read', 400);
+                throw new CustomValidationError('Keywords are required to read');
             }
 
             const wildcardConditions = buildWildcardOptions(params.keys, params.keyword);
@@ -93,15 +85,15 @@ class SampleService extends RootService {
                 query,
                 wildcardConditions
             );
-            if (result.failed) throw new Error(result.error);
+            if (result.failed) throw new CustomControllerError(result.error);
 
             return this.processMultipleReadResults(result);
         } catch (e) {
-            const errorMessage =
-                NODE_ENV === 'DEVELOPMENT'
-                    ? `[${this.serviceName}] readRecordsByWildcard: ${e.message}`
-                    : e.message;
-            const err = this.processFailedResponse(errorMessage, 500);
+            let processedError = this.formatError(this.serviceName, e, 'readRecordsByWildcard');
+            const err = this.processFailedResponse(
+                processedError.errorMessage,
+                processedError.statusCode
+            );
             return next(err);
         }
     }
@@ -109,24 +101,25 @@ class SampleService extends RootService {
     async updateRecordById(request, next) {
         try {
             const { id } = request.params;
-            if (!id) throw new Error('Invalid ID supplied.');
+            if (!id) throw new CustomValidationError('Invalid ID supplied.');
 
             const data = request.body;
-            if (Object.keys(data).length === 0) throw new Error('Update requires data.');
+            if (Object.keys(data).length === 0)
+                throw new CustomValidationError('Update requires data.');
 
             const { error } = updateSchema.validate(data);
             if (error) throw new CustomValidationError(this.filterJOIValidation(error.message));
 
             const result = await this.sampleController.updateRecords({ id }, { ...data });
-            if (result.failed) throw new Error(result.error);
+            if (result.failed) throw new CustomControllerError(result.error);
 
             return this.processUpdateResult(result);
         } catch (e) {
-            const errorMessage =
-                NODE_ENV === 'DEVELOPMENT'
-                    ? `[${this.serviceName}] updateRecordById: ${e.message}`
-                    : e.message;
-            const err = this.processFailedResponse(errorMessage, 500);
+            let processedError = this.formatError(this.serviceName, e, 'updateRecordById');
+            const err = this.processFailedResponse(
+                processedError.errorMessage,
+                processedError.statusCode
+            );
             return next(err);
         }
     }
@@ -134,11 +127,12 @@ class SampleService extends RootService {
     async updateRecords(request, next) {
         try {
             const { options, data } = request.body;
-            if (!options || !data) throw new Error('Invalid options/data', 400);
+            if (!options || !data) throw new CustomValidationError('Invalid options/data');
             if (Object.keys(options).length === 0) {
-                throw new Error('Options are required to update', 400);
+                throw new CustomValidationError('Options are required to update');
             }
-            if (Object.keys(data).length === 0) throw new Error('Data is required to update', 400);
+            if (Object.keys(data).length === 0)
+                throw new CustomValidationError('Data is required to update');
 
             const { seekConditions } = buildQuery(options);
 
@@ -146,15 +140,15 @@ class SampleService extends RootService {
                 { ...seekConditions },
                 { ...data }
             );
-            if (result.failed) throw new Error(result.error);
+            if (result.failed) throw new CustomControllerError(result.error);
 
             return this.processUpdateResult({ ...data, ...result });
         } catch (e) {
-            const errorMessage =
-                NODE_ENV === 'DEVELOPMENT'
-                    ? `[${this.serviceName}] updateRecords: ${e.message}`
-                    : e.message;
-            const err = this.processFailedResponse(errorMessage, 500);
+            let processedError = this.formatError(this.serviceName, e, 'updateRecords');
+            const err = this.processFailedResponse(
+                processedError.errorMessage,
+                processedError.statusCode
+            );
             return next(err);
         }
     }
@@ -162,18 +156,18 @@ class SampleService extends RootService {
     async deleteRecordById(request, next) {
         try {
             const { id } = request.params;
-            if (!id) throw new Error('Invalid ID supplied.');
+            if (!id) throw new CustomValidationError('Invalid ID supplied.');
 
             const result = await this.sampleController.deleteRecords({ id });
-            if (result.failed) throw new Error(result.error);
+            if (result.failed) throw new CustomControllerError(result.error);
 
             return this.processDeleteResult(result);
         } catch (e) {
-            const errorMessage =
-                NODE_ENV === 'DEVELOPMENT'
-                    ? `[${this.serviceName}] deleteRecordById: ${e.message}`
-                    : e.message;
-            const err = this.processFailedResponse(errorMessage, 500);
+            let processedError = this.formatError(this.serviceName, e, 'deleteRecordById');
+            const err = this.processFailedResponse(
+                processedError.errorMessage,
+                processedError.statusCode
+            );
             return next(err);
         }
     }
@@ -181,20 +175,21 @@ class SampleService extends RootService {
     async deleteRecords(request, next) {
         try {
             const { options } = request.body;
-            if (Object.keys(options).length === 0) throw new Error('Options are required', 400);
+            if (Object.keys(options).length === 0)
+                throw new CustomValidationError('Options are required');
 
             const { seekConditions } = buildQuery(options);
 
             const result = await this.sampleController.deleteRecords({ ...seekConditions });
-            if (result.failed) throw new Error(result.error);
+            if (result.failed) throw new CustomControllerError(result.error);
 
             return this.processDeleteResult({ ...result });
         } catch (e) {
-            const errorMessage =
-                NODE_ENV === 'DEVELOPMENT'
-                    ? `[${this.serviceName}] deleteRecords: ${e.message}`
-                    : e.message;
-            const err = this.processFailedResponse(errorMessage, 500);
+            let processedError = this.formatError(this.serviceName, e, 'deleteRecords');
+            const err = this.processFailedResponse(
+                processedError.errorMessage,
+                processedError.statusCode
+            );
             return next(err);
         }
     }

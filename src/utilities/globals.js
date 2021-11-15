18681e0d5113668266e42e801e186d2e9c5f87a9
Sobowale Olayiwola <osobowale@atbtechsoft.com>
2021-11-15 15:22:26 +0100

    Added globals files and caching files

---
+++ src/utilities/globals.js
@@ -0,0 +1,40 @@
+/**
+ * This module handles the globalization of all objects.
+ * This ensures they are properly instantiated into the Global namespace.
+ * @module UTILITY:CustomGlobalization
+ */
+
+const mongoose = require('mongoose');
+const sendEmail = require('./mailing/sendEmail');
+const Controller = require('../controllers/index');
+const { CustomControllerError, CustomValidationError } = require('./customErrors');
+const { hashObject, verifyObject, generateToken, verifyToken } = require('./encryption');
+
+/**
+ * This handles the globalization of all the mongoose models.
+ * This ensures they are properly instantiated into the Global namespace.
+ *
+ */
+const models = mongoose.modelNames();
+for (let i = 0; i < models.length; i++) {
+    global[models[i] + 'Controller'] = new Controller(models[i]);
+}
+
+/**
+ * Custom Error Object globalization
+ */
+global.CustomValidationError = CustomValidationError;
+global.CustomControllerError = CustomControllerError;
+
+/**
+ * Encryption object globalization
+ */
+global.hashObject = hashObject;
+global.verifyObject = verifyObject;
+global.generateToken = generateToken;
+global.verifyToken = verifyToken;
+
+/**
+ * Globalizing sendEmail
+ */
+global.sendEmail = sendEmail;

50f078e508144d8962293d485da2bb800a1e0839
Kolawole Ojo <philliphopeojo@gmail.com>
2021-11-15 10:41:27 +0100

    Events documentation completed

--- src/events/_config.js
+++ src/events/_config.js
@@ -1,13 +1,18 @@
 /**
  *
  * This is the entry-point for all Nodejs Events in the application
- * @module EVENTS
+ * @module EVENTS:Config
  */
 
 const EventEmitter = require('events');
 
 const { Logger } = require('../utilities/logger');
 
+/**
+ *
+ * This class extends the NodeJS event emitter class and allows for using custom events in the application.
+ * @class
+ */
 class AppEvent extends EventEmitter {}
 
 const appEvent = new AppEvent();
--- src/events/_loader.js
+++ src/events/_loader.js
@@ -1,7 +1,17 @@
+/**
+ * This part of the codebase is for handling events loads.
+ * @module EVENTS:Loader
+ */
+
 const { resolve } = require('path');
 
 const glob = require('glob');
 
+/**
+ *
+ * Recursively loads all event definition files in the events folder into the app.
+ * @function
+ */
 function loadEventSystem() {
     const basePath = resolve(__dirname, '.');
     const files = glob.sync('*.js', { cwd: basePath });
